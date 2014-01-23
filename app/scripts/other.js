e56398c4683ec7b206b2f3db3ed53bde87543072
Todd Heidenreich <jtheidenreich@gmail.com>
2014-01-23 08:05:19 -0500

    adding functions

--- app/scripts/other.js
+++ app/scripts/other.js
@@ -25,30 +25,38 @@
 
 // /.+/
 
-var dog = {
-	legs: 4;
-	ears: 2;
-	bark: function function() {
-		console.log ('woof')
-	},
-	countEars: function (){
-		console.log ('I have', this.ears, 'ears');
-	}
-}
+// var dog = {
+// 	legs: 4;
+// 	ears: 2;
+// 	bark: function function() {
+// 		console.log ('woof')
+// 	},
+// 	countEars: function (){
+// 		console.log ('I have', this.ears, 'ears');
+// 	}
+// }
 
 
+//ONE 
 
 var hotdog = {
-	bun [wheat, white, potato]
-	meat [pork, beef, chicken, slurry]
-	toppings [ relish, mustard, sauerkraut]
-
+	bun [wheat, white, potato];
+	meat [pork, beef, chicken, slurry];
+	toppings [ relish, mustard, sauerkraut, ketchup];
 
 }
 
+removeTopping: function (toppings){
+	console.log ('I hate ' + toppings[3]+' on my hotdog!')
+}
+
+
+//TWO
+
 var academy = {
-	teachers [ Joe, Mason]
-	students [ 
+	teachers: [ Joe, Mason];
+	spaces: 15;
+	students: [ 
 			Joshua,
 			Austin,
 			Shane,
@@ -63,15 +71,30 @@ var academy = {
 			Tyler,
 			Jacob, 
 			Elizabeth
-			]
+			];
 }
+
+countStudents: function(){
+	console.log ('There are'+ students.length + "in the Iron Yard Academy")
+}
+
+
+
+
+// THREE
 
 var backpack = {
 	straps: 2;
 	pockets: 7;
+	color: red;
 	brand [ 
 		Nike,
 		Adidas,
 		Jansport
 	]
+}
+
+addPockets: function(){
+	this.pockets = this..pockets +1;
+	console.log (this.pockets + ' is way to many pockets')
 }
