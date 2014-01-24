9117ba089474d2056e13a5f48b0a092774952f11
Todd Heidenreich <jtheidenreich@gmail.com>
2014-01-24 14:03:44 -0500

    still making changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -162,7 +162,7 @@ $('.twenty-three').click(function() {
 //TWENTY-FOUR
 
 $('.twenty-four').click(function(){
-	$(this).toggleClass('.toggle-class');
+	$(this).addClass('change');
 });
 
 
@@ -184,161 +184,30 @@ $('.twenty-six').hover(function() {
 
 //TWENTY-SEVEN
 
+$('.twenty-seven').hover(function() {
+
+	$(this).addClass('super-shadow');	
+});
+
+//TWENTY-EIGHT
 
 
+$('.twenty-eight').hover(function(){
 
+	$(this).addClass('hidden')
+})
 
+//TWENTY-NINE
 
+$('.twenty-nine').hover(function(){
 
+  	$(this).fadeOut(1000);
+});
 
+// THIRTY
 
+$('.thirty').hover(function(){
 
-
-
-
-
-
-
-
-
-
-// $('.activate-box').click(function(){
-//   console.log('the button clicked');
-
-//   $('.box').addClass('active');        
-// });
-
-// // $('.expandable').focus(function(){
-// //         console.log('focused on an input!');
-
-// //         $(this).addClass('error');
-// // })
-
-// $('.submit').click(function(){
-//         console.log('focused on an input');
-
-//         $('.expandable').each(function(){
-//                 if ($(this).val() == '') {
-//                         $(this).addClass('error');
-//                 }
-//         });
-// });
-
-// //Example 1
-// $('.hide-this').click(function(){
-//   $(this).slideUp();
-// });
-
-// //Example 2
-// $('.get-big').hover(function(){
-//   $(this).addClass('get-bigger');
-// });
-
-// //Example 3
-// $('.text-field').mouseenter(function(){
-//   $(this).addClass('mouse-enter');
-// });
-
-// //Example 4
-// $('.change-up').bind( "mouseenter mouseleave", function(){
-//   $(this).toggleClass('change');
-// });
-
-// //Example 5
-// $('.dbl-click-this').dblclick(function(){
-//   $(this).addClass('dbl-click');
-// });
-
-
-
-
-// //Example 9
-// $('.button-trigger').click(function(){
-//   $('.input-trigger').trigger('focus');
-// });
-
-// //Example 10
-// $('.loser').mouseleave(function(){
-//   $(this).removeClass('we-lose');
-// });
-
-// //Example 11
-// $('.hide-me').dblclick(function(){
-//   $(this).hide('slow');
-// });
-
-// //Example 12
-// $('.fade-away').hover(function(){
-//   $(this).fadeOut('fast');
-// });
-
-// //Example 13
-// $('a').click(function(){
-//   $('.pswd').fadeIn(1000,function(){
-//     $('span').fadeIn(100);
-//   });
-//   return false;
-// });
-
-// //Example 14 & 15
-// $('.find-me span').click(function(){
-//   $(this).fadeOut(500, function(){
-//     $('.find-me div').text('"' + $(this).text() + '" has faded out!');
-//     $(this).remove();
-//   });
-// });
-// $('.find-me span').hover(function(){
-//   $(this).addClass('hilite');
-// }, function(){
-//   $(this).removeClass('hilite');
-// });
-
-// //Example 16
-// $('.what-up').dblclick(function(){
-//   alert("Couldn't resist!");
-// });
-
-// //Example 17
-// $('.start-here').mouseup(function(){
-//   if ($('.circle:first').is(':hidden') ) {
-//     $('.circle').slideDown('fast');
-//   } else {
-//     $('.circle').hide();
-//   }  
-// });
-
-// //example 18 & 19
-// $('.question')
-//   .mousedown(function(){
-//     $(this).append("<span style='color:purple;'>A BUTTLOAD!</span>");
-//   })
-//   .mouseup(function(){
-//     $(this).append("<span style='color:red;'> For cereal!</span>");
-//   });
-
-// //example 20 & 21
-// $('.grr p').focusin(function(){
-//   $(this).find('span').css('display','inline').fadeOut(2000);
-// });
-
-// //example 22
-// $('.ur-mom').click(function(){
-//   $('.target').blur();
-// });
-
-// //example 23
-
-// //example 24
-// $('.box-yo').hover(function(){
-//   $(this).addClass('box-yo-move');
-// });
-
-// //example 25
-// $('.switch-aroo').toggle(
-//   function(){
-//     $(this).addClass('switch-ster');
-//   }, function(){
-//     $(this).removeClass('switch-ster');
-//   } 
-// );
+	$(this).addClass('final')
+})
 
